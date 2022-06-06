a5885df6a8b59145f07f5e5b849d48d3fd4ac30b
leoier <leoier.feng@gmail.com>
2022-06-06 17:07:24 -0400

    updated method to load environment vars

--- unitTests.sh
+++ unitTests.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#initialize the test database and run unit tests
+# initialize the test database and run unit tests
+# please update the tokens for director and instructor in setup.sh before running the test
 
 dropdb course_catalog_test --if-exists
 createdb course_catalog_test
 psql course_catalog_test < courseCatalog.psql > /dev/null
+source setup.sh
 python3 test_app.py
