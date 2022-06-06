bb90a3c6ca6e779081ab5a7c65665269b4cd7ab5
leoier <leoier.feng@gmail.com>
2022-06-06 17:10:04 -0400

    script to set up local environment vars

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+# setting up environment variables for local run and testing
+
+export DATABASE_URL='{Your database url here}'
+
+export TEST_DATABASE_URL='{Your test database url here}'
+
+export DIRECTOR_TOKEN='{Generate a token as director}'
+
+export INSTRUCTOR_TOKEN='{Generate a token as instructor}'
